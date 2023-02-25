210fb652123557c613856056204051f23d0f1045
valentina vittoria <valeviit@icloud.com>
2023-02-25 20:24:01 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <button type="button" className="btn btn-danger">Hello</button>
+    </div>
+  )
+}
+
+export default App
